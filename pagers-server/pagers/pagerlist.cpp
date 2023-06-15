6750bc1aedabafd67129f1035adeccaf09817da7
MrJake222 <norbertzpilicy@gmail.com>
2023-06-15 13:40:47 +0200

    fixed bug in pagerlist

--- pagers-server/pagers/pagerlist.cpp
+++ pagers-server/pagers/pagerlist.cpp
@@ -5,10 +5,10 @@ bool PagerList::pager_exists(unsigned short device_id) {
 }
 
 bool PagerList::add_pager(Pager* pager) {
-    if (!pager_exists(pager->get_device_id()))
+    if (pager_exists(pager->get_device_id()))
         return false;
 
-    pagers[pager->get_device_id()] = pager; // copy
+    pagers[pager->get_device_id()] = pager;
     return true;
 }
 
